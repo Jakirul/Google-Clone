01a6f37c1947eabbd3ab5ecbedd038dd070b5eab
Menelaos <mkaskiris@hotmail.com>
2021-11-17 11:59:45 +0000

    added more stuff to db

--- server/data.js
+++ server/data.js
@@ -1,14 +1,14 @@
 const list = [
     {
       search: 'Cats Are Cool',
-      description: 'This website is all about cats and cats are cool',
+      description: 'Wikipedia of cat',
       url: 'https://en.wikipedia.org/wiki/Cat'
     },
     
     {
       search: 'Cats funny videos',
       description: 'Videos all about cats and they are funny!',
-      url: 'https://en.wikipedia.org/wiki/Uk'
+      url: 'https://www.youtube.com/watch?v=pqbBvaBncJs'
     },
     {
       search: 'I love cats',
@@ -18,8 +18,39 @@ const list = [
     {
       search: 'Funny dog video',
       description: 'Videos all about dogs and they are funny!',
+      url: 'https://www.youtube.com/watch?v=W-J-LSWQL-o'
+    }, 
+    {
+      search: 'Dog wikipedia',
+      description: 'Videos all about dogs and they are funny!',
       url: 'https://en.wikipedia.org/wiki/Dog'
-    }
+    },
+    {
+      search: 'Funny dog video',
+      description: 'Videos all about dogs and they are funny!',
+      url: 'https://www.youtube.com/watch?v=W-J-LSWQL-o'
+    }, 
+    {
+      search: 'Dogs trust: Dog Rescue',
+      description: 'Adpot a dog today',
+      url: 'https://www.dogstrust.org.uk/'
+    }, 
+    {
+      search: 'Giraffe',
+      description: 'Exploring the world of food is our passion. Our obsession.',
+      url: 'https://www.giraffe.net/'
+    }, 
+    {
+      search: 'Giraffe - Wikipedia',
+      description: 'The giraffe (Giraffa) is an African artiodactyl mammal, the tallest living terrestrial animal and the largest ruminant. It is traditionally considered to be ...',
+      url: 'https://en.wikipedia.org/wiki/Giraffe'
+    }, 
+    {
+      search: 'Penguin - Wikipedia',
+      description: ' Penguins are a group of aquatic flightless birds. They live almost exclusively in the southern hemisphere: only one ...',
+      url: 'https://en.wikipedia.org/wiki/Penguin'
+    }, 
+
 ]
 
 module.exports = list
