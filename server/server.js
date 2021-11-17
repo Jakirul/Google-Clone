52d2db98f03481a8a45d4bbdddc7a42d7a7f1b51
Menelaos <mkaskiris@hotmail.com>
2021-11-17 10:22:55 +0000

    redirect to random site

--- server/server.js
+++ server/server.js
@@ -4,9 +4,12 @@ const path = require('path')
 const list = require('./data')
 const cors = require('cors');
 
-
 app.use(express.json())
 app.use(cors());
+
+const randomPage = require('./routes/random')
+app.use('/random', randomPage);
+
 app.get('/', (req, res) => {
     res.send(list)
     // res.sendFile((path.join(__dirname, '../client/index.html')))
@@ -18,19 +21,12 @@ app.post('/', (req, res) => {
     res.status(201).send(data)
 })
 
-
-
 app.get('/:searchResult', (req, res) => {
-    let searchTerm = req.params.searchResult;
-
-    // This converts my string (searchTerm) into an array/object. I split it by - because theres no - in my string so it would turn that
-    // entire string into one array value.
-    // Now i could map over it below and use very simple regex to match the title accordingly with the list of searches in ./data
-    const searchTerm2 = searchTerm.toLowerCase().split('-');
+    const searchTerm = [req.params.searchResult.toLowerCase()];
     
     const results = list.filter(item => {
         const title = item.search.toLowerCase();
-        const findWords = searchTerm2.map(term => {
+        const findWords = searchTerm.map(term => {
             // This conerts it to /search term here/
             let myPattern = new RegExp(`${term}`);
             return title.match(myPattern);
